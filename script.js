5f20ce9b7b33e8d6e1a30b29b0fc7ec2a9a19357
Ugur Alp Yildiz <77773407+uguralpyildiz@users.noreply.github.com>
2024-03-27 00:56:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -34,16 +34,3 @@ for (let i = 0; i < navlink.length; i++) {
     
 }
 
-const accordionItem = document.querySelectorAll(".accordion-item");
-
-accordionItem.forEach((el) =>
-
-    el.addEventListener("click", () => {
-        if (el.classList.contains("faq-active")) {
-            el.classList.remove("faq-active");
-        } else {
-            accordionItem.forEach((el) => el.classList.remove("faq-active"));
-            el.classList.add("faq-active");
-        }
-    })
-);
